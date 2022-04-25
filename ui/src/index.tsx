de800b5840fc09181f63e88f6012a63204654635
Patryk Harasiuk <patryk.harasiuk@op.pl>
2022-04-25 22:41:18 +0200

    add workaround to implement createRoot from React v18

--- ui/src/index.tsx
+++ ui/src/index.tsx
@@ -1,7 +1,10 @@
 import * as React from "react";
-import * as ReactDOM from "react-dom";
+import { createRoot } from "react-dom/client";
 
-ReactDOM.render(
-  <React.StrictMode>'hello world'</React.StrictMode>,
-  document.getElementById("root")
-);
+const container = document.getElementById("root");
+
+if (!container) throw new Error("Failed to find the root element");
+
+const root = createRoot(container);
+
+root.render(<React.StrictMode>'hello world'</React.StrictMode>);
